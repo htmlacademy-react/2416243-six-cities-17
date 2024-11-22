9ea1546e3420396e9cab2f094f46347f02e69cb7
Sergei <red00009@outlook.com>
2024-11-21 20:51:02 -0500

    Add main page components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {App} from './components/app/app.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const CITI_COUNT = 5;
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App cityCount={CITI_COUNT}/>
   </React.StrictMode>
 );
