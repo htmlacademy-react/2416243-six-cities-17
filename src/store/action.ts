30dce40310d6eac33f1b5bd9e11ad75058305e72
Sergei <red00009@outlook.com>
2024-12-26 18:38:36 -0500

    Add actions to work with authorization

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,5 +1,6 @@
 import {createAction} from '@reduxjs/toolkit';
 import {CityType, OfferType} from '../types/offer.ts';
+import {AuthorizationStatus} from '../const.ts';
 
 export const Action = {
   LOAD_OFFERS: 'LOAD_OFFERS',
@@ -8,7 +9,8 @@ export const Action = {
   OPEN_SORTING: 'OPEN_SORTING',
   CLOSE_SORTING: 'CLOSE_SORTING',
   RESET_SORTING: 'RESET_SORTING',
-  SET_OFFERS_LOADING_STATUS: 'SET_OFFERS_LOADING_STATUS'
+  SET_OFFERS_LOADING_STATUS: 'SET_OFFERS_LOADING_STATUS',
+  REQUIRE_AUTH: 'REQUIRE_AUTH'
 };
 
 export const loadOffers = createAction<OfferType[]>(Action.LOAD_OFFERS);
@@ -22,3 +24,4 @@ export const openSorting = createAction(Action.OPEN_SORTING);
 export const closeSorting = createAction(Action.CLOSE_SORTING);
 export const resetSorting = createAction(Action.RESET_SORTING);
 export const setOffersLoadingStatus = createAction<boolean>(Action.SET_OFFERS_LOADING_STATUS);
+export const requireAuthorization = createAction<AuthorizationStatus>(Action.REQUIRE_AUTH);
